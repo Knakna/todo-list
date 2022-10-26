0d770bafe6aa194a10dc3eb89ada00ca86d7a777
knakna <kna@tut.by>
2022-10-26 15:40:21 +0300

    add task1

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
 import React from 'react';
 import './App.css';
+import {TaskArray, Todolist} from './Todolist';
 
 function App() {
+    let task1:Array<TaskArray> = [
+        {id: 1, title: "CSS", isDone: true},
+        {id: 2, title: "JS", isDone: false},
+        {id: 3, title: "React", isDone: false}
+    ]
+    let task2: Array<TaskArray> = [
+        {id: 1, title: "CSS", isDone: false},
+        {id: 2, title: "JS", isDone: true},
+        {id: 3, title: "React", isDone: false}
+    ]
+
+
     return (
         <div className="App">
-            <div>
-                <h3>What to learn</h3>
-                <div>
-                    <input/>
-                    <button>+</button>
-                </div>
-                <ul>
-                    <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
-                    <li><input type="checkbox" checked={true}/> <span>JS</span></li>
-                    <li><input type="checkbox" checked={false}/> <span>React</span></li>
-                </ul>
-                <div>
-                    <button>All</button>
-                    <button>Active</button>
-                    <button>Completed</button>
-                </div>
-            </div>
+            <Todolist title="What to learn" tasks={task1}/>
+            <Todolist title="qqq" tasks={task2}/>
         </div>
     );
 }
